d05ccfe7d8f5f5aca0201a29bf1ca92f4df31ba0
cristhian Lopez <cristhian.lopez@tecsup.edu.pe>
2021-04-05 16:35:33 -0500

    first commit

---
+++ Ejercicio03/Ejercicio03/LabelCode.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabelCode : ContentPage
+    {
+        public LabelCode()
+        {
+            InitializeComponent();
+            StackLayout stack = new StackLayout();
+            var underlinelabel = new Label { Text= "This is underline text.",
+                TextDecorations=TextDecorations.Underline };
+            var strikethroughtlabel = new Label { Text = "This is underline text with strikethrought.",
+                TextDecorations=TextDecorations.Strikethrough };
+            var bothlabel = new Label { Text = "This is underline text with strikethrought.",
+                TextDecorations=TextDecorations.Underline | TextDecorations.Strikethrough };
+            stack.Children.Add(underlinelabel);
+            stack.Children.Add(strikethroughtlabel);
+            stack.Children.Add(bothlabel);
+            Content = stack;
+        }
+    }
+}
